e8d4fcc375e54701308dee3173ba766359172647
alinageng <geng.a@northeastern.edu>
2023-10-19 21:47:08 -0400

    fix dashboard

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -8,33 +8,37 @@ import courseImage from "../../img/course_img.jpeg";
 function Dashboard() {
   const courses = db.courses;
   return (
+    <div className="wd-width-100">
         <div className="m-3">
-            <h1 className="">Dashboard</h1>
+            <h1 className="">Dashboard </h1>
             <hr />
-            <div classnName="container">
+            <div className="m-3 wd-courses">
                 <h2>Published Courses</h2>
                 <hr/>
-                <div className="col">
+                <div className="row">
                     {courses.map((course) => (
-                        <div className="card">
+                        <div className="col mt-2">
+                            <div className="card wd-course-card">
                             <img src={courseImage} className="card-img-top" alt="Course Image" />
                             <div className="card-body">
                                 <Link key={course._id} to={`/Kanbas/Courses/${course._id}`} className="list-group-item">
-                                    <h5 class="card-title">{course._id} {course.name}</h5>
-                                    <p class="card-text mb-1 text-secondary">
+                                    <h5 className="card-title">{course._id} {course.name}</h5>
+                                    <p className="card-text mb-1 text-secondary">
                                         {course._id}.{course.number}<br />
                                         {course.startDate} to {course.endDate}
                                     </p>
                                 </Link>
                             </div>     
-                        </div>                  
+                        </div>      
+                        </div>   
+                                   
                     ))
                     }
                 </div>
             </div>
-            
         </div>
-    
+    </div>
+        
   );
 }
 export default Dashboard;
