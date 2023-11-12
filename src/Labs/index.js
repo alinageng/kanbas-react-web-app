f1aa9f848df6f1012b67e2191b725252749e483a
alinageng <geng.a@northeastern.edu>
2023-11-12 18:02:53 -0500

    a5 rest f23

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -2,6 +2,7 @@ import Assignment3 from "./a3";
 import Nav from "../Nav";
 import {Routes, Route, Navigate} from "react-router";
 import Assignment4 from "./a4";
+import Assignment5 from "./a5";
 import store from "./store";
 import {Provider} from "react-redux";
 import React from 'react';
@@ -19,6 +20,8 @@ function Labs() {
                  element={<Assignment3/>}/>
           <Route path="a4"
                  element={<Assignment4/>}/>
+          <Route path="a5"
+                 element={<Assignment5/>}/>
         </Routes>
       </div>
     </Provider>
