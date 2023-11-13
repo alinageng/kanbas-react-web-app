fb72c97e59cb303e8c411417e75be26bd5969e4b
alinageng <geng.a@northeastern.edu>
2023-11-12 21:43:06 -0500

    problem adding courses

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -3,24 +3,42 @@ import {Routes, Route, Navigate} from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import React from 'react';
-import db from "./Database";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import store from "./store";
 import {Provider} from "react-redux";
+import axios from 'axios';
 
 
 function Kanbas() {
-  const [courses, setCourses] = useState(db.courses);
+  const [courses, setCourses] = useState([]);
   const [course, setCourse] = useState({
     name: "New Course", number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15",
   });
-  const addNewCourse = () => {
-    setCourses([...courses, {...course, _id: new Date().getTime().toString()}]);
+  // const addNewCourse = () => {
+  //   setCourses([...courses, {...course, _id: new Date().getTime().toString()}]);
+  // };
+
+  const addCourse = async () => {
+    const response = await axios.post(URL, course);
+
+    setCourses(
+      currentCourses => [response.data, ...currentCourses]);
+    setCourse({ name: "" });
   };
-  const deleteCourse = (courseId) => {
-    setCourses(courses.filter((course) => course._id !== courseId));
+  const deleteCourse = async (course) => {
+    const response = await axios.delete(
+      `${URL}/${course._id}`
+    );
+    setCourses(courses.filter(
+      (c) => c._id !== course._id));
   };
+
+
+
+  // const deleteCourse = (courseId) => {
+  //   setCourses(courses.filter((course) => course._id !== courseId));
+  // };
   const updateCourse = () => {
     setCourses(
       courses.map((c) => {
@@ -32,6 +50,17 @@ function Kanbas() {
       })
     )
   }
+
+
+  const URL = "http://localhost:4000/api/courses";
+  const findAllCourses = async () => {
+    const response = await axios.get(URL);
+    setCourses(response.data);
+  };
+  useEffect(() => {
+    findAllCourses();
+  }, []);
+
 
   return (
     <Provider store={store}>
@@ -50,7 +79,7 @@ function Kanbas() {
                 courses={courses}
                 course={course}
                 setCourse={setCourse}
-                addNewCourse={addNewCourse}
+                addNewCourse={addCourse}
                 deleteCourse={deleteCourse}
                 updateCourse={updateCourse}/>}
             />
